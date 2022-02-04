eb37a33a5a1f8878f23ee8e63643deb33b6a68bb
uliana <ugagina@mail.ru>
2022-02-04 14:43:40 +0500

    изменение дизайна, движения героя, плохие и хорошие растения

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame
+import numpy
 
 width = 450
 height = 400
@@ -8,9 +9,10 @@ height = 400
 pygame.init()
 size = width, height
 screen = pygame.display.set_mode(size)
-fps = 60
+fps = 15
 clock = pygame.time.Clock()
 points = 0
+health = 100
 
 
 def load_image(name, colorkey=None):
@@ -59,22 +61,34 @@ def start_screen():
         clock.tick(fps)
 
 
+def color_surface(surface, red, green, blue):
+    arr = pygame.surfarray.pixels3d(surface)
+    arr[:,:,0] = red
+    arr[:,:,1] = green
+    arr[:,:,2] = blue
+
+
 def draw_points(screen):
     font = pygame.font.Font(None, 30)
-    text = font.render(f"Points: {points}", True, (255, 255, 255))
-    text_x = width - text.get_width() - 10
-    text_y = 10
-    screen.fill((0, 0, 0), (text_x - 5, 0, text_x + text.get_width(), text_y + text.get_height() + 5))
-    screen.blit(text, (text_x, text_y))
+    text_points = font.render(f"Points: {points}", True, (255, 255, 255))
+    text_health = font.render(f"Health: {health}", True, (255, 255, 255))
+    max_width = max(text_points.get_width(), text_health.get_width())
+    text_x = width - max_width - 10
+    text_y = 5
+    screen.fill((0, 0, 0), (text_x - 5, 0, text_x + max_width, text_y + text_points.get_height() * 2 + 5))
+    screen.blit(text_points, (text_x, text_y))
+    screen.blit(text_health, (text_x, text_y + 25))
 
 
 tile_images = {
-    'wall': pygame.transform.scale(load_image('bricks2.png'), (50, 50)),
-    'empty': pygame.transform.scale(load_image('dirt2.png'), (50, 50))
+    'wall': pygame.transform.scale(load_image('tree.png'), (50, 50)),
+    'empty': pygame.transform.scale(load_image('grass1.png'), (50, 50))
 }
 player_image = load_image('mario.png')
-diamond_image = load_image('diamond1.png')
-diamond_image = pygame.transform.scale(diamond_image, (26, 26))
+good_plant_image = load_image('good_plant.png')
+good_plant_image = pygame.transform.scale(good_plant_image, (26, 26))
+bad_plant_image = load_image('bad_plant.png')
+bad_plant_image = pygame.transform.scale(bad_plant_image, (26, 26))
 
 tile_width = tile_height = 50
 
@@ -84,7 +98,7 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(tiles_group, all_sprites)
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
+                tile_width * pos_x, tile_height * pos_y)
 
 
 class Player(pygame.sprite.Sprite):
@@ -108,25 +122,26 @@ class Player(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
-    def update(self, n):
+    def step(self, n=0):
         if n == 1:
-            self.rect = self.rect.move(0, -50)
+            self.rect = self.rect.move(0, -10)
             if pygame.sprite.spritecollideany(self, walls_group):
-                self.rect = self.rect.move(0, 50)
+                self.rect = self.rect.move(0, 10)
         if n == 2:
-            self.rect = self.rect.move(0, 50)
+            self.rect = self.rect.move(0, 10)
             if pygame.sprite.spritecollideany(self, walls_group):
-                self.rect = self.rect.move(0, -50)
+                self.rect = self.rect.move(0, -10)
         if n == 3:
-            self.rect = self.rect.move(-50, 0)
+            self.rect = self.rect.move(-10, 0)
             if pygame.sprite.spritecollideany(self, walls_group):
-                self.rect = self.rect.move(50, 0)
+                self.rect = self.rect.move(10, 0)
         if n == 4:
-            self.rect = self.rect.move(50, 0)
+            self.rect = self.rect.move(10, 0)
             if pygame.sprite.spritecollideany(self, walls_group):
-                self.rect = self.rect.move(-50, 0)
-        self.image_step(n)
-        self.image = self.frames[self.cur_frame]
+                self.rect = self.rect.move(-10, 0)
+        if n:
+            self.image_step(n)
+            self.image = self.frames[self.cur_frame]
 
     def image_step(self, n):
         if self.last_n == n:
@@ -137,18 +152,40 @@ class Player(pygame.sprite.Sprite):
             self.cur_frame = self.dic[n] - 4
         self.last_n = n
 
+    def turn_red(self):
+        orig_surface = self.image
+        orig_surface.convert_alpha()
+        colored_surface = orig_surface.copy()
+        color_surface(colored_surface, 255, 100, 100)
+        self.image = colored_surface
 
-class Diamond(pygame.sprite.Sprite):
+
+class GoodPlant(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
-        super().__init__(diamond_group, all_sprites)
-        self.image = diamond_image
+        super().__init__(plant_group, all_sprites)
+        self.image = good_plant_image
         self.rect = self.image.get_rect().move(
             tile_width * pos_x + 12, tile_height * pos_y + 12)
 
-    def update(self, n):
+    def update(self):
         global points
         if pygame.sprite.spritecollideany(self, player_group):
             points += 1
+            self.kill()
+
+
+class BadPlant(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(plant_group, all_sprites)
+        self.image = bad_plant_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 12, tile_height * pos_y + 12)
+
+    def update(self):
+        global health
+        if pygame.sprite.spritecollideany(self, player_group):
+            health -= 10
+            player.turn_red()
             self.kill()
 
 
@@ -172,17 +209,19 @@ all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 walls_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
-diamond_group = pygame.sprite.Group()
+plant_group = pygame.sprite.Group()
 
 
 def generate_level(level):
     new_player, x, y = None, None, None
-    diamond_coords = []
+    good_plant_coords = []
+    bad_plant_coords = []
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
                 Tile('empty', x, y)
             elif level[y][x] == '#':
+                Tile('empty', x, y)
                 tile = Tile('wall', x, y)
                 walls_group.add(tile)
             elif level[y][x] == '@':
@@ -190,11 +229,16 @@ def generate_level(level):
                 player_y = y
                 Tile('empty', x, y)
             elif level[y][x] == '&':
-                diamond_coords.append([x, y])
+                good_plant_coords.append([x, y])
+                Tile('empty', x, y)
+            elif level[y][x] == '+':
+                bad_plant_coords.append([x, y])
                 Tile('empty', x, y)
     new_player = Player(pygame.transform.scale(load_image('hero.png'), (184, 184)), 4, 4, player_x, player_y)
-    for diamond in diamond_coords:
-        Diamond(diamond[0], diamond[1])
+    for plant in good_plant_coords:
+        GoodPlant(plant[0], plant[1])
+    for plant in bad_plant_coords:
+        BadPlant(plant[0], plant[1])
     return new_player, x, y
 
 
@@ -203,19 +247,26 @@ player, level_x, level_y = generate_level(load_level('map.txt'))
 camera = Camera()
 # start_screen()
 running = True
+motion = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                all_sprites.update(1)
+                motion = 1
             if event.key == pygame.K_DOWN:
-                all_sprites.update(2)
+                motion = 2
             if event.key == pygame.K_LEFT:
-                all_sprites.update(3)
+                motion = 3
             if event.key == pygame.K_RIGHT:
-                all_sprites.update(4)
+                motion = 4
+        if event.type == pygame.KEYUP:
+            if event.key in [pygame.K_LEFT, pygame.K_RIGHT,
+                             pygame.K_UP, pygame.K_DOWN]:
+                motion = 0
+    player.step(motion)
+    all_sprites.update()
     camera.update(player)
     for sprite in all_sprites:
         camera.apply(sprite)
